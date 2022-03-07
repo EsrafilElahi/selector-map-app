f55f5dda2e8d0ad9b47060a7ca27e3f782006599
Esrafil <57289402+EsrafilElahi@users.noreply.github.com>
2022-03-07 11:44:55 +0330

    add all files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Modal from "react-modal";
+import {
+  BrowserView,
+  MobileView,
+  isBrowser,
+  isMobile,
+  isAndroid,
+  isIOS,
+} from "react-device-detect";
 
-function App() {
+import "./App.css";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+const App = () => {
+  useEffect(() => {
+    if (isAndroid) {
+      const url =
+        "intent://instagram.com/#Intent;scheme=https;package=com.instagram.android;end";
+
+      window.location.replace(url);
+    } else if (isIOS) {
+      window.location.replace("instagram://");
+
+      setTimeout(() => {
+        window.location.replace(
+          "https://apps.apple.com/us/app/instagram/id389801252"
+        );
+      }, 10000);
+    } else {
+      window.location.replace("https://instagram.com");
+    }
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <div>
+        If you have not been automatically redirected, click on the following
+        link:
+      </div>
+      {isAndroid ? (
+        <a href="https://play.google.com/store/apps/details?id=com.instagram.android">
+          Open Android app
         </a>
-      </header>
+      ) : isIOS ? (
+        <a href="https://apps.apple.com/us/app/instagram/id389801252">
+          Open iOS app
+        </a>
+      ) : (
+        <a href="https://instagram.com">Open Web app</a>
+      )}
     </div>
   );
-}
+};
 
 export default App;
