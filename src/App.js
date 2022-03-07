20c4c3a9633f826f4350e60b5ce86d61289b86e2
Esrafil <57289402+EsrafilElahi@users.noreply.github.com>
2022-03-07 16:18:40 +0330

    test 8

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div className="container">
       <span>for android map</span>
-      <a href="geo:37.786971,-122.399677" target="_blank">
+      <a href="geo:37.786971,-122.399677" target="_blank" rel="noreferrer">
         open map
       </a>
 
@@ -13,10 +13,15 @@ const App = () => {
       <a
         href="comgooglemaps://?center=40.765819,-73.975866&zoom=14&views=traffic"
         target="_blank"
+        rel="noreferrer"
       >
         open map 1
       </a>
-      <a href="comgooglemaps://?center=3.1357,101.6880&zoom=14" target="_blank">
+      <a
+        href="comgooglemaps://?center=3.1357,101.6880&zoom=14"
+        target="_blank"
+        rel="noreferrer"
+      >
         open map 2
       </a>
     </div>
