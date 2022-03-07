905ff43bf278c42dca96e44f316d9bed74f3e5f2
Esrafil <57289402+EsrafilElahi@users.noreply.github.com>
2022-03-07 13:13:40 +0330

    test 2

--- src/App.js
+++ src/App.js
@@ -9,19 +9,18 @@ import {
 } from "react-device-detect";
 import "./App.css";
 
-
-
 // window.location = 'geo:40.765819,-73.975866'
 
 const App = () => {
   useEffect(() => {
+    // android
     if (isAndroid) {
       const url =
-        // "intent://instagram.com/#Intent;scheme=https;package=com.instagram.android;end";
         "intent://maps.google.com/#Intent;scheme=https;package=com.google.android.apps.maps;end";
-
       window.location.replace(url);
-    } else if (isIOS) {
+    }
+    // ios
+    else if (isIOS) {
       window.location.replace("instagram://");
 
       setTimeout(() => {
@@ -30,7 +29,7 @@ const App = () => {
         );
       }, 10000);
     } else {
-      window.location.replace("https://instagram.com");
+      // window.location.replace("https://www.google.com/maps");
     }
   }, []);
 
@@ -43,28 +42,49 @@ const App = () => {
   };
 
   return (
-    <div className="App">
+    <div className="container">
       <BrowserView>
         <h1>This is rendered only in browser</h1>
       </BrowserView>
       <MobileView>
         <h1>This is rendered only on mobile</h1>
       </MobileView>
-      <div>{isMobile && "mobiles"}</div>
-      <div>{isBrowser && "browsers"}</div>
+      <div>{isMobile && "is mobile view"}</div>
+      <div>{isBrowser && "is browser view"}</div>
       <button onClick={mapsSelector}>open map</button>
 
       {isAndroid ? (
-        <a href="https://play.google.com/store/apps/details?id=com.instagram.android">
-          Open Android app
+        <a
+          href="https://play.google.com/store/apps/details?id=com.instagram.android"
+          target="_blank"
+        >
+          open google play google map
         </a>
       ) : isIOS ? (
-        <a href="https://apps.apple.com/us/app/instagram/id389801252">
-          Open iOS app
+        <a
+          href="https://apps.apple.com/id/app/google-maps-transit-food/id585027354"
+          target="_blank"
+        >
+          open apple store google map
         </a>
       ) : (
-        <a href="https://instagram.com">Open Web app</a>
+        <a
+          href="https://maps.google.com/maps"
+          target="_blank"
+        >
+          open browser google map
+        </a>
       )}
+
+      <a href="https://balad.ir/#10/35.6725/51.4181" target="_blank">
+        balad browser map
+      </a>
+      <a
+        href="https://neshan.org/maps/@35.689200,51.389000,11.0z,0.0p"
+        target="_blank"
+      >
+        neshan browser map
+      </a>
     </div>
   );
 };
